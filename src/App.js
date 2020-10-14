1a1db4da65f1898ffb9fb713c4fd3ed15304540f
Oliver Dahl <oliverpdahl@gmail.com>
2020-10-14 20:48:40 +0000

    Added navbar

--- src/App.js
+++ src/App.js
@@ -3,9 +3,21 @@ import './App.css';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
+import Navbar from 'react-bootstrap/Navbar';
 function App() {
   return (
     <div className="text-center" style={{height:1000}}>
+      <Navbar bg="info" variant="info">
+        <Navbar.Brand href="#home">
+          <img
+            alt=""
+            src="/home/codespace/workspace/fidenso-flights/public/fidenso logo.png"
+            height="30"
+            className="d-inline-block align-top"
+          />{' '}
+          Fidesco
+        </Navbar.Brand>
+      </Navbar>
       <div className="m-4">
       <Jumbotron>
         <Container>
